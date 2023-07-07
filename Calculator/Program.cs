079a19f3233840bd7cd6b557d8bfddff125ff0fc
rugby8724 <jeremiah.d.wise@gmail.com>
2023-07-07 12:29:37 -0500

    Add Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿
+StartCalculator();
+
+void StartCalculator()
+{
+    Console.WriteLine("Hello!");
+    Console.WriteLine("Input the first number: ");
+
+    var firstNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine("Input the second number: ");
+    var secondNumber = int.Parse(Console.ReadLine());
+    var mathOperation = "";
+    while(mathOperation == "" || mathOperation == "bad response")
+    {
+        mathOperation = MathmaticalOperations();
+    }
+    var total = Calculate(firstNumber, secondNumber, mathOperation);
+    Console.WriteLine(total);
+    Console.WriteLine(" Press any key to exit program");
+    Console.ReadKey();
+    
+
+}
+
+string MathmaticalOperations()
+{
+    Console.WriteLine("What would operation would you like to do? ");
+    Console.WriteLine("[A]dd");
+    Console.WriteLine("[S]ubtract");
+    Console.WriteLine("[M]ultiply");
+    var mathOperation = Console.ReadLine().ToLower();
+    if(mathOperation == "a" || mathOperation == "s" || mathOperation == "m")
+    {
+        return mathOperation;
+    }
+    return "bad response";
+
+}
+string Calculate(int a, int b, string oper)
+{
+    string op;
+    int total;
+    if(oper == "a")
+    {
+        op = " + ";
+        total = a + b;
+        
+    }
+    else if(oper == "s")
+    {
+        op = " - ";
+        total = a - b;
+    }
+    else
+    {
+        op = " * ";
+        total = a * b;
+    }
+    return a + op + b + " = " + total;
+}
--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -1,3 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Provide a number");
+string userInput = Console.ReadLine();
 
+int number = int.Parse(userInput);
+
+Console.WriteLine(number);
+
+
+
+
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("[S]ee all TODOs");
+//Console.WriteLine("[A]dd a TODO");
+//Console.WriteLine("[R]emove a TODO");
+//Console.WriteLine("[E]xit");
+
+
+//var userChoice = Console.ReadLine();
+
+//bool isLong = IsLong(userChoice);
+//Console.WriteLine(isLong);
+
+//if(userChoice == "S")
+//{
+//    PrintSelectedOption("See all TODOs");
+//}
+//else if (userChoice == "A")
+//{
+//    PrintSelectedOption("Add a TODO");
+//}
+//else if (userChoice == "R")
+//{
+//    PrintSelectedOption("Remove a TODO");
+//}
+//else if (userChoice == "E")
+//{
+//    PrintSelectedOption("Exit");
+//}
+
+
+
+
+//Console.ReadKey();
+
+//void PrintSelectedOption(string selectedOption)
+//{
+//    Console.WriteLine("Selected option " + selectedOption);
+//}
+
+//bool IsLong(string userChoice)
+//{
+//   return userChoice.Length >= 10;
+    
+//}
+
+
+
+// non-void method
+//int Add(int a, int b)
+//{
+//    return a + b;
+//}
